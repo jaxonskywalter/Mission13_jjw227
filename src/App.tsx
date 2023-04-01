8bf8d7dc92337933eca70ecb60846efc21ac7780
jaxonskywalter <jaxonskywalter@gmail.com>
2023-04-01 00:12:13 -0600

    test

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Welcome extends React.Component {
+  render() {
+    return <h1>The Joel Hilton Film Collection</h1>;
+  }
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Welcome />
+      <img src="JoelHiltonHeadshot.jpg" alt="Joel Hilton"></img>
     </div>
   );
 }
