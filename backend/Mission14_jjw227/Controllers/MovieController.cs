da17d52fd75beff0fd40e8402118f66e8390d9bf
jaxonskywalter <jaxonskywalter@gmail.com>
2023-04-23 00:20:42 -0600

    adding backend

---
+++ backend/Mission14_jjw227/Controllers/MovieController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission14_jjw227.Data;
+
+namespace Mission14_jjw227.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : Controller
+    {
+        private MoviesDbContext context;
+
+        public MovieController (MoviesDbContext temp)
+        {
+            context = temp;
+        }
+
+        public IEnumerable<Movies> Get()
+        {
+            var x = context.Movies.ToArray();
+
+            return context.Movies.ToArray();
+        }
+    }
+}
---
+++ backend/Mission14_jjw227/Data/MoviesDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mission14_jjw227.Data
+{
+    public class MoviesDbContext : DbContext
+    {
+        public MoviesDbContext (DbContextOptions<MoviesDbContext> options) : base(options)
+        { 
+        
+        }
+
+        public DbSet<Movies> Movies { get; set; }
+    }
+}
